de31b12e73d52cf0d34234563bb695691968ca0c
hema1mathi <hemasantoshimathi@gmail.com>
2025-04-12 23:52:18 +0530

    NewsLetter and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import {Route,Routes, useLocation} from 'react-router-dom'
 import Home from './pages/Home'
 import {Toaster} from "react-hot-toast";
+import Footer from './components/Footer';
 const App = () => {
 
 const isSellarPath= useLocation().pathname.includes("seller");
@@ -23,6 +24,7 @@ const isSellarPath= useLocation().pathname.includes("seller");
 </Routes>
 
       </div>
+      {!isSellarPath && <Footer/>}
     </div>
   )
 }
