b84d7a6534389940de85d76b55e639872e383a00
Muskaan <muskanyadavonline@gmail.com>
2025-04-14 14:06:11 +0530

    API creation

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from 'cors';
 import connectDB from "./configs/db.js";
 import 'dotenv/config';
+import userRouter from "./routes/userRoute.js";
 
 
 const app= express();
@@ -24,6 +25,10 @@ app.use(cors({origin:allowedOrigins, credentials:true}));
 
 
 app.get('/',(req,res)=>res.send("API is working"));
+
+app.use('/api/user',userRouter)
+
+
 
 app.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
