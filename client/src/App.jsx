5824b0f6e79e081e029380b9764faad6dcc57aae
Mansi8588 <ajmrhw12@gmail.com>
2025-04-14 09:03:33 +0530

    Add product on seller side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,19 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import MyOrders from './pages/MyOrders'
 import { AddAddress } from './pages/AddAddress'
+import AllProducts from './pages/AllProducts'
+import Orders from './pages/seller/Orders'
 import {Route,Routes, useLocation} from 'react-router-dom'
 import Home from './pages/Home'
 import {Toaster} from "react-hot-toast";
 import Footer from './components/Footer';
 import { useAppContext } from './context/AppContext';
 import Login from './components/Login';
-import AllProducts from './pages/AllProducts';
+import AddProduct from './pages/seller/AddProduct';
 import ProductCategory from './pages/ProductCategory';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
+import {ProductList} from './pages/seller/ProductList';
 import SellerLogin from './components/seller/SellerLogin';
 import SellerLayout from './pages/seller/SellerLayout'
 const App = () => {
@@ -37,7 +40,14 @@ const {showUserLogin,isSeller} = useAppContext()
   <Route path='/cart' element= {<Cart/>} />
   <Route path='/add-address' element= {<AddAddress/>} />
   <Route path='/my-orders' element= {<MyOrders/>} />
-  <Route path='/seller' element= {isSeller?<SellerLayout/>:<SellerLogin/>}/>
+  <Route path='/seller' element= {isSeller?<SellerLayout/>:<SellerLogin/>}>
+   <Route index element={isSeller ?<AddProduct/>:null} />
+   <Route path='product-list' element={<ProductList/>} />
+   <Route path='orders' element={<Orders/>} />
+
+
+
+  </Route>
 
 </Routes>
 
