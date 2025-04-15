2d1b12022978d6fe805e977670a50f0515c62400
hema1mathi <hemasantoshimathi@gmail.com>
2025-04-16 00:51:17 +0530

    get and add the addresses

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import connectCloudinary from "./configs/cloudinary.js";
 import productRouter from "./routes/productRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import addressRouter from "./routes/addressRoute.js";
 
 
 const app= express();
@@ -34,6 +35,7 @@ app.use('/api/user',userRouter)
 app.use('/api/seller',sellerRouter)
 app.use('/api/product',productRouter)
 app.use('/api/cart',cartRouter)
+app.use('/api/address',addressRouter)
 
 
 
